9637aaf59d9e2efec73cc711c0f012d865f43ba1
Silvia Yaguchi <silviayaguchi@gmail.com>
2018-05-10 22:57:18 -0300

    Add Stopwatch and minor styling with Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Clock from './Clock';
+import Stopwatch from './Stopwatch';
 import "./App.css";
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: "December 25, 2017",
-      newDeadlilne: ''
+      deadline: "December 25, 2018",
+      newDeadlilne: '',
+      min: 0,
+      sec: 0,
+      time: 0,
+      endTime: 0
     };
   }
 
@@ -17,18 +23,48 @@ class App extends Component {
     });
   }
 
+  startStopwatch() {
+    this.setState({
+      time: (this.state.min*60 + Number(this.state.sec))*1000
+    }, function() {
+      const endTime = new Date().getTime() + this.state.time;
+      this.setState({endTime: endTime}, function() {
+          console.log(this.state.endTime);
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
         <Clock deadline={this.state.deadline}/>
-        <input
-          placeholder="new date"
-          onChange={event => this.setState({newDeadline: event.target.value})}
-      />
-        <button onClick={() => this.changeDeadline()}>Submit</button>
+        <Form inline>
+          <FormControl className="Deadline-input"
+            placeholder="new date"
+            onChange={event => this.setState({newDeadline: event.target.value})}
+        />
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        </Form>
+
+        <div className="App-title">
+          Stopwatch
+        </div>
+        <Form inline>
+          <FormControl  className='Stopwatch-input min'
+          placeholder="min"
+          onChange={event => this.setState({min: event.target.value})}
+          />
+          <FormControl  className='Stopwatch-input sec'
+          placeholder="sec"
+          onChange={event => this.setState({sec: event.target.value})}
+          />
+          <Button onClick={() => this.startStopwatch()}>Start</Button>
+        </Form>
+        <Stopwatch time={this.state.time} endTime={this.state.endTime}/>
       </div>
     );
+    // <Stopwatch time={{minutes: this.state.min, seconds: this.state.sec}}/>
   }
 }
 
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -10,15 +10,45 @@ class Clock extends Component {
       minutes: 0,
       seconds: 0
     }
-    console.log(this.props);
   }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    //After the first run, run the method again every second
+  }
+
+  leading0(num) {
+    return num < 10 ? '0' + num : num; //Using ternary expression to shorten conditional
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/(1000 * 60)) % 60);
+    const hours = Math.floor((time/(1000 * 60 * 60)) % 24);
+    const days = Math.floor(time/(1000 * 60 * 60 * 24));
+
+    if(time > 0) {
+      this.setState({days, hours, minutes, seconds});
+    } else {
+      this.setState({days: 0, hours: 0, minutes: 0, seconds: 0});
+    }
+    //If the state propertie and the variable have the same name it tis allowed to write
+    //the name only once
+  }
+
   render() {
     return(
       <div>
-        <div className="clock-days">{this.state.days} days</div>
-        <div className="clock-hours">{this.state.hours} Hours</div>
-        <div className="clock-minutes">{this.state.minutes}</div>
-        <div className="clock-seconds">{this.state.second}</div>
+        <div className="clock-days">{this.leading0(this.state.days)} days</div>
+        <div className="clock-hours">{this.leading0(this.state.hours)} Hours</div>
+        <div className="clock-minutes">{this.leading0(this.state.minutes)}</div>
+        <div className="clock-seconds">{this.leading0(this.state.seconds)}</div>
       </div>
     );
   }
